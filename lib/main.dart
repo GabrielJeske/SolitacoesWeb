f185cc26cd074bca33b9b0edd8e889806c0c8df6
Gabriel <gabriel.r.jeske@gmail.com.br>
2026-05-13 11:26:29 -0400

    Adicionando estrutura do servidor http em Go

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:solicitacoes/funcoes/login.dart';
 import 'package:solicitacoes/routes.dart';
 import 'package:solicitacoes/store/arquivos_mob.dart';
 import 'package:solicitacoes/store/login_mob.dart';
